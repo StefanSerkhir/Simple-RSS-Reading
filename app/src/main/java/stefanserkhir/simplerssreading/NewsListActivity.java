8340451ba03e8f342a446eda794d30c0fafe4e11
Stefan Serkhir <stefan.serkhir@gmail.com>
2020-06-20 18:55:06 +0300

    Created base app UI

---
+++ app/src/main/java/stefanserkhir/simplerssreading/NewsListActivity.java
@@ -0,0 +1,14 @@
+package stefanserkhir.simplerssreading;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewsListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_list);
+    }
+}
